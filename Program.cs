d0b9847b432c2734af8e232fdc6fe0203b39e6a0
Jojoshua <jojoshua@gmail.com>
2022-02-10 08:25:50 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Playwright;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            await Go().ConfigureAwait(false);
+        }
+
+        public static async Task Go()
+        {
+            using (var playwright = await Playwright.CreateAsync())
+            {
+                var browser = await playwright.Chromium.LaunchAsync();
+                var page = await browser.NewPageAsync();
+                await page.GotoAsync("https://playwright.dev/dotnet");
+                await page.ScreenshotAsync(new PageScreenshotOptions { Path = "screenshot.png" });
+                await browser.DisposeAsync();
+            }
+        }
+    }
+}
